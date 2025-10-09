ce94ac45147b856f62cf9387b77201a955599de2
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2025-10-09 23:01:21 +0800

    Charts: render immediately with fixed container height to ensure visibility

--- src/components/SuperAdmin/DashboardChartsLazy.jsx
+++ src/components/SuperAdmin/DashboardChartsLazy.jsx
@@ -49,7 +49,7 @@ const DashboardChartsLazy = memo(({
           <div className="panel-heading">
             <h3 className="panel-title">Patient Growth</h3>
           </div>
-          <div className="panel-body">
+          <div className="panel-body" style={{height: 300, position: 'relative'}}>
             <LineChart data={patientsChartData} options={lineChartOptions} />
           </div>
         </div>
@@ -59,7 +59,7 @@ const DashboardChartsLazy = memo(({
           <div className="panel-heading">
             <h3 className="panel-title">Case Distribution</h3>
           </div>
-          <div className="panel-body">
+          <div className="panel-body" style={{height: 300, position: 'relative'}}>
             <BarChart data={casesChartData} options={barChartOptions} />
           </div>
         </div>
@@ -69,7 +69,7 @@ const DashboardChartsLazy = memo(({
           <div className="panel-heading">
             <h3 className="panel-title">Vaccine Stock Levels</h3>
           </div>
-          <div className="panel-body">
+          <div className="panel-body" style={{height: 300, position: 'relative'}}>
             <LineChart data={vaccinesChartData} options={vaccinesChartOptions} />
           </div>
         </div>
@@ -79,7 +79,7 @@ const DashboardChartsLazy = memo(({
           <div className="panel-heading">
             <h3 className="panel-title">Case Severity</h3>
           </div>
-          <div className="panel-body">
+          <div className="panel-body" style={{height: 300, position: 'relative'}}>
             <PieChart data={severityChartData} options={severityChartOptions} />
           </div>
         </div>
